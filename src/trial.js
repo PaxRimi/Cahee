86a4869b73c109e09a9ae503ee332860b8aacb71
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-16 18:31:35 +0200

    Add first section to the project

---
+++ src/trial.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Trial extends React.Component{
+    render(){
+        return(
+            <section className="trial">
+                <div className="container">
+                    <div className="trial_txt">
+                        <h2>
+                            Fresh Life
+                        </h2>
+                        <p>
+                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                        </p>
+                        <a href="#">GET FREE NOW</a>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
+}
+
+export default Trial;
