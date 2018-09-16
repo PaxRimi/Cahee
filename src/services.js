2485325411b4d1998b4cf81dcce28f1fd8c1299d
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-16 22:35:15 +0200

    Create service section

---
+++ src/services.js
@@ -0,0 +1,59 @@
+import React from "react";
+import trolley from "./images/trolley.png"
+import sun from "./images/sun.png"
+import earth from "./images/earth.png"
+import carrot from "./images/carrot.png"
+
+class Services extends React.Component{
+    render(){
+        return(
+            <section className="services">
+                <div className="container">
+                    <div className="services_title">
+                        <h2>
+                            Our Services
+                        </h2>
+                    </div>
+                    <div className="services_list">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        Online Shopping
+                                    </span>
+                                    <img src={trolley}/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        Quality Product
+                                    </span>
+                                    <img src={sun}/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        Domestic & International Delivery
+                                    </span>
+                                    <img src={earth}/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        Well Organized
+                                    </span>
+                                    <img src={carrot}/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Services;
