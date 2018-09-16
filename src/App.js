12f92f85e611f70de778587e407991830f4c1c9f
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-16 18:33:44 +0200

    Add first section to App file and style them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./header";
+import Trial from "./trial"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Trial/>
       </div>
     );
   }
