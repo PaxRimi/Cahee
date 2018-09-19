45732de79b73bc81da6fd6b84142594a834aca96
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-19 16:44:37 +0200

    Add blog section to the project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./about";
 import Services from "./services";
 import Gallery from "./gallery";
 import Subscription from "./subscription";
+import Blog from "./blog";
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
         <Services/>
         <Gallery/>
         <Subscription/>
+        <Blog/>
       </div>
     );
   }
