4fb5289042338feb076d7ab12953906f090df174
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-09-20 12:53:05 +0200

    Add function to show and hide menu onphones

--- src/header.js
+++ src/header.js
@@ -2,6 +2,22 @@ import React from "react";
 
 
     class Header extends React.Component{
+
+        handleMenuClick = () => {
+          let menu = document.querySelector("nav");
+          if(menu.style.display === "block"){
+              menu.style.display = "none";
+          } else {
+              menu.style.display = "block";
+          }
+        };
+
+        handleLinkClick = () => {
+            let menu = document.querySelector("nav");
+
+            menu.style.display = "none";
+        };
+
         render(){
             return (
                 <header>
@@ -11,15 +27,39 @@ import React from "react";
                         </span>
                         <nav>
                             <ul>
-                                <li>HOME</li>
-                                <li>ABOUT</li>
-                                <li>SERVICES</li>
-                                <li>GALLERY</li>
-                                <li>BLOG</li>
-                                <li>CONTACT</li>
+                                <li>
+                                    <a href="#trial" onClick={this.handleLinkClick}>
+                                        HOME
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#about" onClick={this.handleLinkClick}>
+                                        ABOUT
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#services" onClick={this.handleLinkClick}>
+                                        SERVICES
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#gallery" onClick={this.handleLinkClick}>
+                                    GALLERY
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#blog" onClick={this.handleLinkClick}>
+                                    BLOG
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#contact" onClick={this.handleLinkClick}>
+                                    CONTACT
+                                    </a>
+                                </li>
                             </ul>
                         </nav>
-                        <i className="fas fa-bars"/>
+                        <i className="fas fa-bars" onClick={this.handleMenuClick}/>
                     </div>
                 </header>
             )
